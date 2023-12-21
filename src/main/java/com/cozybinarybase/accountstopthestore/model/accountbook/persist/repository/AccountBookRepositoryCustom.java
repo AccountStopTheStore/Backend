8068547ada8fc52e18d0594ec68767664e4c4c03
lazyflavor <46445338+lazyflavor@users.noreply.github.com>
2023-12-21 16:17:06 +0900

    QueryDSL로 검색 기능 구현.

--- src/main/java/com/cozybinarybase/accountstopthestore/model/accountbook/persist/repository/AccountBookRepositoryCustom.java
+++ src/main/java/com/cozybinarybase/accountstopthestore/model/accountbook/persist/repository/AccountBookRepositoryCustom.java
@@ -1,7 +1,16 @@
 package com.cozybinarybase.accountstopthestore.model.accountbook.persist.repository;
 
+import com.cozybinarybase.accountstopthestore.model.accountbook.persist.entity.AccountBookEntity;
+import java.time.LocalDate;
 import javax.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountBookRepositoryCustom {
   EntityManager getEntityManager();
+
+  Page<AccountBookEntity> searchByCriteria(String keyword, LocalDate startDate, LocalDate endDate,
+      String categoryName, Long minPrice, Long maxPrice,
+      Long memberId, Pageable pageable);
 }
